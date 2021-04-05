3e037514111ffe26c68dae64da36660600c05470
Christopher N. KATOYI <me@christopher2k.dev>
2021-04-05 12:29:46 +0200

    :sparkles: project header component

---
+++ src/components/ProjectHeader.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  export let title: string
+  export let categories: string[]
+  export let year: string
+</script>
+
+<header>
+  <h1>{title}</h1>
+  <h2>{categories.join(', ')}, {year}</h2>
+</header>
+
+<style lang="scss">
+  @import 'theme.scss';
+
+  header {
+    width: 70%;
+    max-width: 500px;
+    padding-left: 3.5rem;
+    padding-top: 3.5rem;
+    margin-bottom: 6rem;
+  }
+
+  h1 {
+    font-family: $primary-font;
+    font-weight: normal;
+    font-size: 1.7rem;
+    line-height: 2.465rem;
+    text-transform: uppercase;
+    color: $black;
+  }
+
+  h2 {
+    font-family: $primary-font;
+    font-weight: normal;
+    font-size: 1.6rem;
+    line-height: 2.32rem;
+    color: $black;
+  }
+</style>
