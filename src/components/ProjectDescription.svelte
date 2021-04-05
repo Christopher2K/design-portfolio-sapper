f1acfae70f03947966e9a755690a3010cd3b1095
Christopher N. KATOYI <me@christopher2k.dev>
2021-04-05 12:36:33 +0200

    :sparkles: project description

---
+++ src/components/ProjectDescription.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  export let langAcronym: string
+  export let htmlContent: string
+</script>
+
+<article>
+  <span>{langAcronym}</span>
+  <div>
+    {@html htmlContent}
+  </div>
+</article>
+
+<style lang="scss">
+  @import 'theme.scss';
+
+  article {
+    display: grid;
+    grid-template-columns: 2.5rem minmax(0, 1fr);
+    width: 100%;
+    column-gap: 1.4rem;
+    margin-bottom: 30px;
+  }
+
+  span,
+  div {
+    font-family: $primary-font;
+    font-weight: normal;
+    font-size: 1.7rem;
+    line-height: 2.465rem;
+  }
+</style>
